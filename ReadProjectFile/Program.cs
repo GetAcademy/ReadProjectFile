67287e2d27bd69bfe324b9139a90bf75e5b1ac50
GET Academy <terje@kolderup.net>
2020-04-28 09:38:56 +0200

    Initial commit

---
+++ ReadProjectFile/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace ReadProjectFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(File.ReadAllText("Assets/Textfiles/Textfile1.txt"));
+        }
+    }
+}
